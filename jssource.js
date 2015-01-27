badc7273641db2216a0a896f11690ab72564d369
iNohn <o4008at1203@126.com>
2015-01-28 00:03:50 +0800

    Changes to be committed: 	new file:   csssource.css 	new file:   jssource.js 	new file:   this.html

---
+++ jssource.js
@@ -0,0 +1,16 @@
+var qsbutton = document.getElementById("qs");
+// var sqbutton = document.getElementById("sq");
+var repo = document.getElementById("repo");
+qsbutton.onclick = function(){
+	linearray = repo.value.split("\n");
+	res = "";
+	var leng = linearray.length;
+	for (var i=0; i<leng; ++i) {
+		linearray[i] = "'" + linearray[i];
+		resarray = linearray[i].split(" ");
+		res = res + resarray[0]+" "+resarray[1];
+		if (i-leng+1) res = res + "\r\n";
+	}
+	repo.value = res;
+	alert("已将qq词库转换为搜狗可用的词库。");
+}
